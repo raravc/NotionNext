a664446247a34837fdab44af9282825a6969eddb
raravc <1007436491@qq.com>
2025-02-03 21:08:47 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,8 @@
-import BLOG from '@/blog.config'
+/** import BLOG from '@/blog.config'
 import { siteConfig } from '@/lib/config'
 import { getGlobalData } from '@/lib/db/getSiteData'
 import { DynamicLayout } from '@/themes/theme'
 
-/**
- * 404
- * @param {*} props
- * @returns
- */
 const NoFound = props => {
   const theme = siteConfig('THEME', BLOG.THEME, props.NOTION_CONFIG)
   return <DynamicLayout theme={theme} layoutName='Layout404' {...props} />
@@ -21,3 +16,18 @@ export async function getStaticProps(req) {
 }
 
 export default NoFound
+*/
+
+
+// pages/404.js
+export default function Custom404() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '100px' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>页面不存在，请返回首页.</p>
+      <a href="/" style={{ color: 'blue', textDecoration: 'underline' }}>
+        回到首页
+      </a>
+    </div>
+  )
+}
